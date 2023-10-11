6ca00d7b2f982ac04a0b2170a87c7a09196c03a7
Aleksei Zhuikov <design.azv@gmail.com>
2023-10-11 23:06:28 +0300

    add app

---
+++ README.md
@@ -0,0 +1,15 @@
+### Простой поиск с пагинацией
+
+**В данном примере было реализовано:**
+
+- получение данных `comments` c [JSONplaceholder](https://jsonplaceholder.typicode.com/) и динамическая их отрисовка.
+- Для быстрого поиска, была сделана фильтрация по `email` пользователей.
+- Для удобства, при фильтрации, было добавлено выделение каждой найденной буквы заданным цветом.
+- Добавлена пагинация с динамическим выводом количества страниц
+
+**Стек использованный в работе:**
+
+<img src="https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=JavaScript&logoColor=black" />
+  <img src="https://img.shields.io/badge/HTML-E34F26?style=for-the-badge&logo=HTML5&logoColor=white" />
+<img src="https://img.shields.io/badge/CSS-2162AE?style=for-the-badge&logo=CSS3&logoColor=white" />
+<img src="https://img.shields.io/badge/bootstrap-7952B3?style=for-the-badge&logo=bootstrap&logoColor=white" />
---
+++ app.js
@@ -0,0 +1,118 @@
+
+const listEl = document.querySelector('#list')
+const inputEl = document.querySelector('#filter')
+const paginationEl = document.querySelector('#container-pagination')
+const counterEl = document.querySelector('#counter')
+const prevEl = document.querySelector('#prev')
+const nextEl = document.querySelector('#next')
+
+let COMMENTS = []
+let totalCountComments = null
+const numberCommentsPerPage = 5
+let pageNumber = 1
+let numberOfPages = null
+
+// Фильтрация по email
+function filterChar() {
+    const valueChar = this.value
+    const filteredComments = COMMENTS.filter(comment => {
+        return comment.email.toLowerCase().includes(this.value.toLowerCase())
+    })
+    render(filteredComments, valueChar)
+}
+
+// Вешаем слушатели на Input и на блок pagination
+inputEl.addEventListener('input', filterChar)
+paginationEl.addEventListener('click', changePrevNext)
+
+// Устанавливаем значения кнопок prev next
+function setCountPage(prevNum, nexNum) {
+    return counterEl.innerHTML = `${prevNum} / ${nexNum}`
+}
+
+// Изменяем значения кнопок prev next
+function changePrevNext(e) {
+    e.preventDefault()
+    if (e.target.id === 'prev' && pageNumber > 1) {
+        // if (pageNumber > 1) {
+        pageNumber--
+        setCountPage(pageNumber, numberOfPages)
+        inputEl.value = ''
+        start(pageNumber)
+        // }
+    }
+    if (e.target.id == 'next' && pageNumber < numberOfPages) {
+        // if (pageNumber < numberOfPages) {
+        pageNumber++
+        setCountPage(pageNumber, numberOfPages)
+        inputEl.value = ''
+        start(pageNumber)
+        // }
+    }
+}
+
+// Получение комментов API
+async function start() {
+    listEl.innerHTML = 'Загружаем комментарии . . .'
+    paginationEl.classList.add('d-none')
+    try {
+        const resp = await fetch(
+            `https://jsonplaceholder.typicode.com/comments?_page=${pageNumber}&_limit=${numberCommentsPerPage}`
+        )
+        totalCountComments = resp.headers.get('X-Total-Count')
+        numberOfPages = Math.ceil(totalCountComments / numberCommentsPerPage)
+        setCountPage(pageNumber, numberOfPages)
+        let data = await resp.json()
+
+        // имитируем задержку сервера
+        setTimeout(() => {
+            COMMENTS = data
+            render(COMMENTS)
+        }, 100)
+
+    } catch (error) {
+        listEl.style.color = 'red'
+        listEl.innerHTML = error.message
+    }
+
+    // условия для disabled button prev, next
+    if (pageNumber >= 2) {
+        prevEl.removeAttribute('disabled')
+    } else {
+        prevEl.setAttribute('disabled', true)
+    }
+    if (pageNumber >= numberOfPages) {
+        nextEl.setAttribute('disabled', true)
+    } else {
+        nextEl.removeAttribute('disabled')
+    }
+
+}
+start()
+
+// Рендер комментов
+function render(comments = [], valueChar = null) {
+    if (!comments.length) {
+        listEl.innerHTML = 'Пользователя с таким e-mail нет'
+        paginationEl.classList.add('d-none')
+
+    } else {
+        paginationEl.classList.remove('d-none')
+        const html = comments.map(comment => {
+            const regex = new RegExp(`${valueChar}`, 'gi')
+            const findEmail = comment.email.replace(regex,
+                (match) => `<span class="bg-warning">${match}</span>`
+            )
+            return `
+                <li class="list-group-item pt-3">
+                <span class="font-monospace">${findEmail}</span>
+                <p class="font-monospace">id: ${comment.id}</p>
+                    <p class="mt-2">${comment.body}</p>
+                </li>
+                    `
+        }).join('')
+
+        listEl.innerHTML = html
+    }
+}
+
